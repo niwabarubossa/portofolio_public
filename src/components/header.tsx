d7ad72e7569a14126b63066dc81b2bad17ff5702
Ryogo <niwabarubossa@yahoo.co.jp>
2021-10-11 18:39:31 +0900

    header add

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
+
+import styled from "styled-components";
+
+function Header() {
+    return (
+        <Grid xs={12}>
+            <HeaderWrapper>
+                <HeaderItem><Link to="/">TOP</Link></HeaderItem>
+                <HeaderItem><Link to="/profile">PROFILE</Link></HeaderItem>
+                <HeaderItem><Link to="/photo">PHOTO</Link></HeaderItem>
+            </HeaderWrapper>
+        </Grid>
+    );
+}
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+  font-weight: 800;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border-bottom: solid 1px #bbbbbb;
+`
+
+const HeaderItem = styled.div`
+    margin: 0 10px;
+    color:#444444;
+`
