9285e7b17627a7691ed9845c0a7778ea87602d5a
Ryogo <niwabarubossa@yahoo.co.jp>
2022-03-06 21:57:31 +0900

    fix typo

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -5,7 +5,7 @@ export default function Achievements() {
             Achievements
             <div>
                 国際学会（査読あり）<br />
-                <li>EMS-Supported Throwing:Preliminary investigation on EMS-supported training of movement form, Ryogo Niwa, Kazuya Izumi, Shieru Suzuki, and Yoichi Ochiai, Human Computer Interactoin Internationao 2022.<br /></li>
+                <li>EMS-Supported Throwing:Preliminary investigation on EMS-supported training of movement form, Ryogo Niwa, Kazuya Izumi, Shieru Suzuki, and Yoichi Ochiai, Human Computer Interactoin International 2022<br /></li>
                 コンペ
                 <li>M5 Stack Japan Creativity Contest 2021 特別賞「イマジナリー賞」<a href="ttps://campusgenius.jp/2021/exhibition/play/7/1 ">リンク</a></li>
             </div>
