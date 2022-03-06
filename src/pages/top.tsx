052e3daa2fe7098ec6267cbe3b89fd82f8e27303
Ryogo <niwabarubossa@yahoo.co.jp>
2022-03-06 21:53:27 +0900

    achieementを追加

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,10 +1,12 @@
 import Photo from "./photo"
 import Profile from "./profile"
+import Achievements from "./achievements"
 
 export default function Top() {
   return (
     <>
       <Profile />
+      <Achievements />
       <Photo />
     </>
   )
